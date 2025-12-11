f34ba5012d0835f436cacba349df350790d2b244
SheepRain-code <yrain0221@gmail.com>
2025-12-11 21:50:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets load correctly on any repo path
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
